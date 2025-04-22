0c6ec128c9fd0f71e1b111fc8697c69cbee3bf9c
Hyejin Kim <khyejin08@naver.com>
2025-04-22 14:48:03 +0900

    main-slider 슬라이더 기능 구현

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".main-slider__content-group", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
