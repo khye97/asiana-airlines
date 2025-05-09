2f40cece97f0c63fb96b6f4554510d2a5fb51ed8
Hyejin Kim <khyejin08@naver.com>
2025-05-09 18:41:35 +0900

    modal calendar 기능 구현

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// flight-services-widget ----------------------------------------
+// flight-services-widget --------------------------------------------------
 $(".fsw").load("/include/booking-form.html", function () {
   // 전체 tab 변경 함수
   function changeTabPanel(target) {
@@ -55,5 +55,169 @@ $(".fsw").load("/include/booking-form.html", function () {
   // 노선조회/편명조회 탭 변경
   $(".tp-flight-status__ctrl").on("click", function () {
     changeDetailTab($(this), "flight-status");
+  });
+
+  // modal --------------------------------------------------------------------
+  // modal 닫기
+  $(".fsw-modal__close, .overlay").on("click", function () {
+    $(".fsw-modal").removeClass("is-active");
+    $(".overlay").removeClass("is-active");
+
+    resetCityAirport();
+    resetCalendar();
+  });
+
+  // city/airport -------------------------------------------------------------
+  // modal 열기 - 출발지
+  $(".fsw__tp-field-area.flight-origin").on("click", function () {
+    $(".fsw-modal__airport .fsw-modal__header-title").text("출발지 선택");
+    $(".fsw-modal__airport").addClass("is-active");
+    $(".overlay").addClass("is-active");
+  });
+
+  // modal 열기 - 도착지
+  $(".fsw__tp-field-area.flight-destination").on("click", function () {
+    $(".fsw-modal__airport .fsw-modal__header-title").text("도착지 선택");
+    $(".fsw-modal__airport").addClass("is-active");
+    $(".overlay").addClass("is-active");
+  });
+
+  // modal 내부 도시/공항 변경 함수
+  function filterRegion(e) {
+    let targetRegion = $(e.target).data("region");
+    let selectedElement = $(".fsw-modal__city-airport-wrap").filter(function (
+      index,
+      element
+    ) {
+      let region = $(element).data("region");
+      return region === targetRegion ? element : undefined;
+    });
+
+    return selectedElement;
+  }
+
+  // 출발지/도착지 reset 함수
+  function resetCityAirport() {
+    $(".fsw-modal__select-region.is-active").removeClass("is-active");
+    $(".fsw-modal__select-region[data-region=korea]").addClass("is-active");
+    $(".fsw-modal__city-airport-wrap.is-active").removeClass("is-active");
+    $(".fsw-modal__city-airport-wrap[data-region=korea]").addClass("is-active");
+    $(".fsw-modal__select-city-airport.is-active").removeClass("is-active");
+  }
+
+  // modal 내부 도시/공항 변경
+  $(".fsw-modal__select-region").on("click", function (e) {
+    $(".fsw-modal__select-region.is-active").removeClass("is-active");
+    $(this).addClass("is-active");
+    $(".fsw-modal__city-airport-wrap.is-active").removeClass("is-active");
+
+    let el = filterRegion(e);
+    el.addClass("is-active");
+
+    $(".fsw-modal__select-city-airport.is-active").removeClass("is-active");
+  });
+
+  // 도시/공항 개별 선택
+  $(".fsw-modal__select-city-airport").on("click", function () {
+    $(".fsw-modal__select-city-airport.is-active").removeClass("is-active");
+    $(this).addClass("is-active");
+  });
+
+  // calendar --------------------------------------------------------------------
+  // modal 열기 - calendar
+  $(".fsw__tp-field-area.departure-date, .fsw__tp-field-area.return-date").on(
+    "click",
+    function () {
+      $(".fsw-modal__calendar").addClass("is-active");
+      $(".overlay").addClass("is-active");
+
+      if ($(this).data("trip") === "round-trip") {
+        console.log("왕복임");
+        $(".fsw-modal__return-date").css("display", "inline-block");
+        $(".fsw-modal__calendar").attr("data-calendar-type", "round-trip");
+      } else {
+        console.log("편도임");
+        $(".fsw-modal__return-date").css("display", "none");
+        $(".fsw-modal__calendar").attr("data-calendar-type", "one-way");
+      }
+    }
+  );
+
+  let fromDayIndex;
+  let returnDayIndex;
+
+  // dayList 배열 생성 함수
+  function pushDayList(fromDayIndex, returnDayIndex) {
+    let dayList = [];
+
+    for (let i = fromDayIndex + 4; i <= returnDayIndex + 4; i++) {
+      dayList.push($(".fsw-modal__calendar-table td")[i]);
+    }
+
+    $.each(dayList, function (index, item) {
+      $(item).addClass("middle");
+    });
+  }
+
+  // 캘린더 상단 날짜 값 설정 함수
+  function setDateValue(target, className) {
+    let dateValue = target.children("input").val();
+    $(`.fsw-modal__${className}-date`).text(dateValue);
+  }
+
+  // 캘린더 날짜 설정 reset 함수
+  function resetCalendar() {
+    $(".from-day").removeClass("from-day");
+    $(".return-day").removeClass("return-day");
+    $(".middle").removeClass("middle");
+    $(".fsw-modal__calendar-item button.date").css("pointer-events", "auto");
+    $(".fsw-modal__departure-date").text("가는 날");
+    $(".fsw-modal__return-date").text("오는 날");
+  }
+
+  // 왕복 티켓 날짜 선택 기능
+  function setRoundTripDate(target) {
+    let fromDay = $(".fsw-modal__calendar-table td").filter(function (
+      index,
+      element
+    ) {
+      return $(element).hasClass("from-day") ? element : undefined;
+    });
+
+    // 오는 날
+    if (fromDay.length >= 1) {
+      target.parents("td").addClass("return-day");
+      returnDayIndex = $(".fsw-modal__calendar-item button.date").index(target);
+
+      pushDayList(fromDayIndex, returnDayIndex);
+      setDateValue(target, "return");
+
+      $(".fsw-modal__calendar-item button.date").css("pointer-events", "none");
+    } else {
+      // 가는 날
+      target.parents("td").addClass("from-day");
+      fromDayIndex = $(".fsw-modal__calendar-item button.date").index(target);
+
+      setDateValue(target, "departure");
+    }
+  }
+
+  // 캘린더 날짜 클릭 기능
+  $(".fsw-modal__calendar-item button.date").on("click", function () {
+    let calendarType = $(".fsw-modal__calendar").attr("data-calendar-type");
+    if (calendarType === "round-trip") {
+      // 왕복
+      setRoundTripDate($(this));
+    } else {
+      // 편도
+      $(this).parents("td").addClass("from-day");
+      $(".fsw-modal__calendar-item button.date").css("pointer-events", "none");
+      setDateValue($(this), "departure");
+    }
+  });
+
+  // reset 버튼
+  $(".fsw-modal__btn-reset").on("click", function () {
+    resetCalendar();
   });
 });
