fee26dd81172e3f9201367660525f404c99415c2
Hyejin Kim <khyejin08@naver.com>
2025-05-21 13:45:39 +0900

    flight-deals (추천 항공권) 슬라이더 기능 구현

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".main-slider__content-group", {
+var swiper = new Swiper(".main-slider__slide-list", {
   pagination: {
     el: ".swiper-pagination",
     type: "fraction",
@@ -6,5 +6,29 @@ var swiper = new Swiper(".main-slider__content-group", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper2 = new Swiper(".flight-deals__slide-list", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
   },
 });
