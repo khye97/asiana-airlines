dda985f29e5376c3b415c3d2c54aa972c881ca5f
Hyejin Kim <khyejin08@naver.com>
2025-06-03 19:21:00 +0900

    booking-form js 버그 수정

--- js/booking-form.js
+++ js/booking-form.js
@@ -62,24 +62,40 @@ $(".fsw").load("/include/booking-form.html", function () {
   $(".fsw-modal__close, .overlay").on("click", function () {
     $(".fsw-modal").removeClass("is-active");
     $(".overlay").removeClass("is-active");
-
-    resetCityAirport();
-    resetCalendar();
   });
 
   // city/airport -------------------------------------------------------------
+  // 출발지/도착지 reset 함수
+  function resetCityAirport() {
+    $(".fsw-modal__select-region.is-active").removeClass("is-active");
+    $(".fsw-modal__select-region[data-region=korea]").addClass("is-active");
+    $(".fsw-modal__city-airport-wrap.is-active").removeClass("is-active");
+    $(".fsw-modal__city-airport-wrap[data-region=korea]").addClass("is-active");
+    $(".fsw-modal__select-city-airport.is-active").removeClass("is-active");
+  }
+
+  let clickedBtnLocation;
+
   // modal 열기 - 출발지
   $(".fsw__tp-field-area.flight-origin").on("click", function () {
+    clickedBtnLocation = $(this);
     $(".fsw-modal__airport .fsw-modal__header-title").text("출발지 선택");
+    $(".fsw-modal__airport").attr("data-location", "origin");
     $(".fsw-modal__airport").addClass("is-active");
     $(".overlay").addClass("is-active");
+
+    resetCityAirport();
   });
 
   // modal 열기 - 도착지
   $(".fsw__tp-field-area.flight-destination").on("click", function () {
+    clickedBtnLocation = $(this);
     $(".fsw-modal__airport .fsw-modal__header-title").text("도착지 선택");
+    $(".fsw-modal__airport").attr("data-location", "destination");
     $(".fsw-modal__airport").addClass("is-active");
     $(".overlay").addClass("is-active");
+
+    resetCityAirport();
   });
 
   // modal 내부 도시/공항 변경 함수
@@ -96,16 +112,7 @@ $(".fsw").load("/include/booking-form.html", function () {
     return selectedElement;
   }
 
-  // 출발지/도착지 reset 함수
-  function resetCityAirport() {
-    $(".fsw-modal__select-region.is-active").removeClass("is-active");
-    $(".fsw-modal__select-region[data-region=korea]").addClass("is-active");
-    $(".fsw-modal__city-airport-wrap.is-active").removeClass("is-active");
-    $(".fsw-modal__city-airport-wrap[data-region=korea]").addClass("is-active");
-    $(".fsw-modal__select-city-airport.is-active").removeClass("is-active");
-  }
-
-  // modal 내부 도시/공항 변경
+  // modal 내부 지역 변경
   $(".fsw-modal__select-region").on("click", function (e) {
     $(".fsw-modal__select-region.is-active").removeClass("is-active");
     $(this).addClass("is-active");
@@ -123,11 +130,70 @@ $(".fsw").load("/include/booking-form.html", function () {
     $(this).addClass("is-active");
   });
 
+  // 도시/공항 확인 버튼
+  $(".fsw-modal__airport .fsw-modal__btn-confirm").on("click", function () {
+    let cityName = $(".fsw-modal__city-airport-name.is-active")
+      .children(".city-name")
+      .text();
+    let airportCode = $(".fsw-modal__city-airport-name.is-active")
+      .children(".airport-code")
+      .text();
+    if (cityName === "" || airportCode === "") {
+      alert("지역 또는 공항을 선택하세요.");
+    } else {
+      clickedBtnLocation.html(`${airportCode} ${cityName}`);
+
+      $(".fsw-modal").removeClass("is-active");
+      $(".overlay").removeClass("is-active");
+    }
+
+    if (clickedBtnLocation.hasClass("flight-origin")) {
+      departureAirport = airportCode + " " + cityName;
+    } else if (clickedBtnLocation.hasClass("flight-destination")) {
+      destinationAirport = airportCode + " " + cityName;
+    }
+  });
+
+  // switch 버튼
+  let reverse = null;
+  let departureAirport;
+  let destinationAirport;
+
+  $(".fsw__tp-switch-location").on("click", function () {
+    let departureBtn = $(this).prev();
+    let destinationBtn = $(this).next();
+
+    if (reverse === null) {
+      departureBtn.text(destinationAirport);
+      destinationBtn.text(departureAirport);
+      reverse = "reverse";
+    } else {
+      departureBtn.text(departureAirport);
+      destinationBtn.text(destinationAirport);
+      reverse = null;
+    }
+  });
+
   // calendar --------------------------------------------------------------------
   // modal 열기 - calendar
+  let clickedBtnCalendar;
+
+  // 캘린더 날짜 설정 reset 함수
+  function resetCalendar() {
+    $(".from-day").removeClass("from-day");
+    $(".return-day").removeClass("return-day");
+    $(".middle").removeClass("middle");
+    $(".fsw-modal__calendar-item button.date").css("pointer-events", "auto");
+    $(".fsw-modal__departure-date").text("가는 날");
+    $(".fsw-modal__return-date").text("오는 날");
+  }
+
+  // 가는 날/오는 날/탑승일 모달 열기
   $(
     ".fsw__tp-field-area.departure-date, .fsw__tp-field-area.return-date, .boarding-date"
   ).on("click", function () {
+    clickedBtnCalendar = $(this);
+
     $(".fsw-modal__calendar").addClass("is-active");
     $(".overlay").addClass("is-active");
 
@@ -140,6 +206,38 @@ $(".fsw").load("/include/booking-form.html", function () {
     } else {
       $(".fsw-modal__return-date").css("display", "none");
       $(".fsw-modal__calendar").attr("data-calendar-type", "one-way");
+    }
+
+    resetCalendar();
+  });
+
+  // 모달 내부 확인 버튼
+  $(".fsw-modal__calendar .fsw-modal__btn-confirm").on("click", function () {
+    let tripData = clickedBtnCalendar.data("trip");
+
+    if (tripData === "round-trip") {
+      let departureDate = $(".fsw-modal__calendar.is-active")
+        .find(".fsw-modal__departure-date")
+        .text();
+      let returnDate = $(".fsw-modal__calendar.is-active")
+        .find(".fsw-modal__return-date")
+        .text();
+
+      if (departureDate === "가는 날" || returnDate === "오는 날") {
+        alert("가는 날, 오는 날을 모두 선택해주세요.");
+      } else {
+        $(".tp-flight-booking__round-trip .departure-date").text(departureDate);
+        $(".tp-flight-booking__round-trip .return-date").text(returnDate);
+        $(".fsw-modal").removeClass("is-active");
+        $(".overlay").removeClass("is-active");
+      }
+    } else {
+      let departureDate = $(".fsw-modal__calendar.is-active")
+        .find(".fsw-modal__departure-date")
+        .text();
+      clickedBtnCalendar.text(departureDate);
+      $(".fsw-modal").removeClass("is-active");
+      $(".overlay").removeClass("is-active");
     }
   });
 
@@ -165,16 +263,6 @@ $(".fsw").load("/include/booking-form.html", function () {
     $(`.fsw-modal__${className}-date`).text(dateValue);
   }
 
-  // 캘린더 날짜 설정 reset 함수
-  function resetCalendar() {
-    $(".from-day").removeClass("from-day");
-    $(".return-day").removeClass("return-day");
-    $(".middle").removeClass("middle");
-    $(".fsw-modal__calendar-item button.date").css("pointer-events", "auto");
-    $(".fsw-modal__departure-date").text("가는 날");
-    $(".fsw-modal__return-date").text("오는 날");
-  }
-
   // 왕복 티켓 날짜 선택 기능
   function setRoundTripDate(target) {
     let fromDay = $(".fsw-modal__calendar-table td").filter(function (
@@ -197,6 +285,18 @@ $(".fsw").load("/include/booking-form.html", function () {
       // 가는 날
       target.parents("td").addClass("from-day");
       fromDayIndex = $(".fsw-modal__calendar-item button.date").index(target);
+      let $allTd = $(".fsw-modal__calendar-table td");
+      let arr = [];
+
+      for (let i = 0; i < fromDayIndex; i++) {
+        arr.push($allTd[i + 4]);
+      }
+
+      arr.forEach(function (item) {
+        $(item).children("button").css({
+          "pointer-events": "none",
+        });
+      });
 
       setDateValue(target, "departure");
     }
@@ -221,11 +321,8 @@ $(".fsw").load("/include/booking-form.html", function () {
     resetCalendar();
   });
 
-  // modal 열기 - 탑승객
-  $(".fsw__tp-field-area.passenger-count").on("click", function () {
-    $(".fsw-modal__passenger").addClass("is-active");
-    $(".overlay").addClass("is-active");
-  });
+  // modal - passenger-count ---------------------------
+  let clickedBtnPassenger;
 
   // modal - 탑승객
   const $adultForm = $("[data-passenger-type=adult]");
@@ -247,6 +344,48 @@ $(".fsw").load("/include/booking-form.html", function () {
   let adultNum = +$adultNumElement.text();
   let childNum = +$childNumElement.text();
   let infantNum = +$infantNumElement.text();
+
+  // modal 열기 - 탑승객
+  $(".fsw__tp-field-area.passenger-count").on("click", function () {
+    clickedBtnPassenger = $(this);
+
+    adultNum = 1;
+    childNum = 0;
+    infantNum = 0;
+    updateDisplay();
+    refreshButtons();
+
+    $(".fsw-modal__passenger").addClass("is-active");
+    $(".overlay").addClass("is-active");
+  });
+
+  // 탑승객 확인 버튼
+  $(".fsw-modal__passenger .fsw-modal__btn-confirm").on("click", function () {
+    let arr = [adultNum, childNum, infantNum];
+    let massage = [];
+
+    if (adultNum === 0 && childNum === 0) {
+      alert("성인 혹은 소아를 1명 이상 선택하세요.");
+      return;
+    }
+
+    arr.forEach(function (item, index) {
+      let passenger =
+        index === 0
+          ? `성인 ${adultNum}`
+          : index === 1
+          ? `소아 ${childNum}`
+          : `유아 ${infantNum}`;
+
+      if (item != 0) {
+        massage.push(`${passenger}`);
+      }
+    });
+
+    clickedBtnPassenger.text(massage.join(", "));
+    $(".fsw-modal").removeClass("is-active");
+    $(".overlay").removeClass("is-active");
+  });
 
   // 버튼 disable 처리 함수
   function PasseChangeDisabled($btn, isDisabled) {
@@ -314,10 +453,58 @@ $(".fsw").load("/include/booking-form.html", function () {
   // 초기 상태 세팅
   refreshButtons();
 
-  // modal 열기 - 좌석 등급
+  // modal 열기 - 좌석 등급 -------------------------------------------
+  let clickedBtnSeat;
+
+  // 모달 열기
   $(".fsw__tp-field-area.seat-grade-option").on("click", function () {
+    clickedBtnSeat = $(this);
     $(".fsw-modal__seat-grade").addClass("is-active");
     $(".overlay").addClass("is-active");
+    let clickedBtnData = clickedBtnSeat
+      .parents(".fsw__tp-field-group")
+      .data("ctrl-panel");
+
+    seatGradeReset();
+
+    if (clickedBtnData === "one-way" || clickedBtnData === "multi-city") {
+      toggleCheckbox("#separate-round-trip", true);
+    }
+  });
+
+  // 좌석 등급 모달 확인 버튼
+  $(".fsw-modal__seat-grade .fsw-modal__btn-confirm").on("click", function () {
+    let $departure = $(
+      ".fsw-modal__seat-grade-form-wrap[data-flight-type=departure]"
+    );
+    let $return = $(
+      ".fsw-modal__seat-grade-form-wrap[data-flight-type=return]"
+    );
+    let $roundTrip = $(
+      ".fsw-modal__seat-grade-form-wrap[data-flight-type=round-trip]"
+    );
+
+    if ($("#separate-round-trip").is(":checked")) {
+      let seatGradeDeparture = $departure
+        .find(".fsw-modal__seat-grade-btn-option.is-selected")
+        .text()
+        .trim();
+      let seatGradeReturn = $return
+        .find(".fsw-modal__seat-grade-btn-option.is-selected")
+        .text()
+        .trim();
+      let text = `${seatGradeDeparture}, ${seatGradeReturn}`;
+      clickedBtnSeat.text(text);
+    } else {
+      let seatGrade = $roundTrip
+        .find(".fsw-modal__seat-grade-btn-option.is-selected")
+        .text()
+        .trim();
+      clickedBtnSeat.text(seatGrade);
+    }
+
+    $(".fsw-modal").removeClass("is-active");
+    $(".overlay").removeClass("is-active");
   });
 
   let btnOption = ".fsw-modal__seat-grade-btn-option";
@@ -326,6 +513,7 @@ $(".fsw").load("/include/booking-form.html", function () {
   let $departureForm = $roundForm;
   let $returnForm = $(`${btnWrap}[data-flight-type=return`);
 
+  // 유틸 함수
   // is-selected, <i> 제거 함수
   function seatClearClass($wrap) {
     $wrap.find(`${btnOption}.is-selected i`).remove();
@@ -341,6 +529,10 @@ $(".fsw").load("/include/booking-form.html", function () {
   // 버튼 disabled 처리 함수
   function seatToggleDisabled($element, bool) {
     $element.prop("disabled", bool);
+    let textColor = bool ? "gray" : "body";
+    $($element).css({
+      color: `var(--${textColor}-text)`,
+    });
   }
 
   // 체크박스 disabled 처리 함수
@@ -350,23 +542,6 @@ $(".fsw").load("/include/booking-form.html", function () {
       .next("label")
       .css({ color: `var(--${bool ? "gray" : "body"}-text)` });
   }
-
-  // 체크박스: 업그레이드 가능한 좌석만 보기
-  $("#upgrade-only").on("change", function () {
-    if ($(this).is(":checked")) {
-      $(`${btnOption}`).prop("disabled", true);
-      let $economyStandardBtn = $roundForm.find(
-        `${btnOption}[data-seat-grade=economy-standard]`
-      );
-      seatClearClass($roundForm);
-      seatAddClass($economyStandardBtn);
-      toggleCheckbox("#separate-round-trip", true);
-      $economyStandardBtn.prop("disabled", false);
-    } else {
-      $(".fsw-modal__seat-grade-btn-option").prop("disabled", false);
-      toggleCheckbox("#separate-round-trip", false);
-    }
-  });
 
   // 출도착편 다른 등급 선택 화면에서 등급 버튼 클릭 기능
   function clickDifferentBtn($target) {
@@ -381,11 +556,48 @@ $(".fsw").load("/include/booking-form.html", function () {
 
     seatClearClass($(`${btnWrap}[data-flight-type=${thisPartType}]`));
     seatAddClass($target);
-    $otherAllBtns.prop("disabled", false);
+    seatToggleDisabled($otherAllBtns, false);
     seatToggleDisabled($otherSameBtn, true);
   }
 
-  // 좌석 등급 옵션 버튼 클릭
+  // 좌석 등급 reset 함수
+  function seatGradeReset() {
+    toggleCheckbox("#upgrade-only", false);
+    toggleCheckbox("#separate-round-trip", false);
+    $("#upgrade-only").prop("checked", false);
+    $("#separate-round-trip").prop("checked", false);
+    $departureForm.attr("data-flight-type", "round-trip");
+    $roundForm.children(".fsw-modal__seat-grade-form-title").text("출도착편");
+    $returnForm.hide();
+    seatToggleDisabled(
+      $roundForm.find(".fsw-modal__seat-grade-btn-option[disabled]"),
+      false
+    );
+    seatClearClass($roundForm);
+    seatAddClass(
+      $roundForm.find(`${btnOption}[data-seat-grade=economy-standard]`)
+    );
+  }
+
+  // 체크박스: 업그레이드 가능한 좌석만 보기
+  $("#upgrade-only").on("change", function () {
+    if ($(this).is(":checked")) {
+      let $economyStandardBtn = $roundForm.find(
+        `${btnOption}[data-seat-grade=economy-standard]`
+      );
+
+      toggleCheckbox("#separate-round-trip", true);
+      seatToggleDisabled($(`${btnOption}`), true);
+      seatToggleDisabled($economyStandardBtn, false);
+      seatClearClass($roundForm);
+      seatAddClass($economyStandardBtn);
+    } else {
+      toggleCheckbox("#separate-round-trip", false);
+      seatToggleDisabled($(`${btnOption}`), false);
+    }
+  });
+
+  // 모달 내 세부 클릭: 좌석 등급 옵션 버튼 클릭
   $(".fsw-modal__seat-grade-btn-option").on("click", function () {
     let type = $(this).parents(btnWrap).attr("data-flight-type");
 
@@ -409,13 +621,11 @@ $(".fsw").load("/include/booking-form.html", function () {
       .attr("data-seat-grade");
 
     if ($(this).is(":checked")) {
-      // checked이면 가는편/오는편 선택이 활성화 됨
       toggleCheckbox("#upgrade-only", true);
       $roundForm.children(".fsw-modal__seat-grade-form-title").text("가는 편");
       $roundForm.attr("data-flight-type", "departure");
       $(`${btnWrap}[data-flight-type="return"]`).show();
 
-      // departure의 is-selected 버튼과 return의 is-selected 버튼이 같은 경우
       if (departureSelectedGrade === returnSelectedGrade) {
         seatToggleDisabled(
           $returnForm.find(
@@ -425,6 +635,9 @@ $(".fsw").load("/include/booking-form.html", function () {
         );
         seatClearClass($returnForm);
         seatAddClass($returnForm.find(`${btnOption}:not(:disabled)`).first());
+        $returnForm
+          .find(`${btnOption}:not(:disabled)`)
+          .css("color", "var(--body-text)");
         returnSelectedGrade = $returnForm
           .find(`${btnOption}.is-selected`)
           .attr("data-seat-grade");
@@ -443,6 +656,10 @@ $(".fsw").load("/include/booking-form.html", function () {
         ),
         true
       );
+      let notDisabledBtns = $returnForm.find(btnOption).filter(function () {
+        return !$(this).prop("disabled");
+      });
+      notDisabledBtns.css("color", "var(--body-text)");
     } else {
       // 출도착 편
       toggleCheckbox("#upgrade-only", false);
